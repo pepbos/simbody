ddbc6f21300a1ab5d0a7fa5e13d5961b67e9621a
pepbos <pepijnvandenbos@gmail.com>
2024-04-19 13:00:53 +0200

    add WrappingPathSubsystem functions to cpp

--- Simbody/include/simbody/internal/Wrapping.cpp
+++ Simbody/include/simbody/internal/Wrapping.cpp
@@ -5,6 +5,56 @@
 using namespace SimTK;
 
 //==============================================================================
+//            SUBSYSTEM
+//==============================================================================
+
+bool WrappingPathSubsystem::isInstanceOf(const Subsystem& s) {
+    return Impl::isA(s.getSubsystemGuts());
+}
+
+const WrappingPathSubsystem& WrappingPathSubsystem::
+downcast(const Subsystem& s) {
+    assert(isInstanceOf(s));
+    return static_cast<const WrappingPathSubsystem&>(s);
+}
+WrappingPathSubsystem& WrappingPathSubsystem::
+updDowncast(Subsystem& s) {
+    assert(isInstanceOf(s));
+    return static_cast<WrappingPathSubsystem&>(s);
+}
+
+const WrappingPathSubsystem::Impl& WrappingPathSubsystem::
+getImpl() const {
+    return SimTK_DYNAMIC_CAST_DEBUG<const Impl&>(getSubsystemGuts());
+}
+WrappingPathSubsystem::Impl& WrappingPathSubsystem::
+updImpl() {
+    return SimTK_DYNAMIC_CAST_DEBUG<Impl&>(updSubsystemGuts());
+}
+
+// Create Subsystem but don't associate it with any System. This isn't much use
+// except for making std::vectors, which require a default constructor to be 
+// available.
+WrappingPathSubsystem::WrappingPathSubsystem() 
+{   adoptSubsystemGuts(new Impl()); }
+
+WrappingPathSubsystem::WrappingPathSubsystem(MultibodySystem& mbs) 
+{   adoptSubsystemGuts(new Impl());
+    mbs.adoptSubsystem(*this); } // steal ownership
+
+int WrappingPathSubsystem::getNumPaths() const
+{   return getImpl().getNumPaths(); }
+
+const WrappingPath& WrappingPathSubsystem::
+getPath(WrappingPathIndex cableIx) const
+{   return getImpl().getCablePath(cableIx); }
+
+WrappingPath& WrappingPathSubsystem::
+updPath(WrappingPathIndex cableIx)
+{   return updImpl().updCablePath(cableIx); }
+
+
+//==============================================================================
 //            WRAPPING PATH
 //==============================================================================
 
